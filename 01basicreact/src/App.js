dce17f3d3a098e933963be9654a3ad8aba54c4ef
Devotion <letsmaildipak@gmail.com>
2024-04-12 03:17:57 +0530

    learned to set up react app using both traditional method and using vite too

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Chai Aur React | Dipak</h1>;
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Chai aur react with vite | Dipak</h1>;
+}
+
+export default App;
